e999f6312d20b09fcd7e9970c6eb20e048c7e140
kons77 <staneua@gmail.com>
2025-02-23 23:39:03 +0200

    Save the token to the database

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -282,6 +282,13 @@ func (app *application) CreateAuthToken(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
+	//save to db
+	err = app.DB.InsertToken(token, user)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
 	// send response
 
 	var payload struct {
