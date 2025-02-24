19fbda3fadd2c20a5534afe3eb4289dea3b103d0
kons77 <staneua@gmail.com>
2025-02-24 22:45:31 +0200

    Save the token to browser local storage

--- internal/models/tokens.go
+++ internal/models/tokens.go
@@ -45,13 +45,21 @@ func (m *DBModel) InsertToken(t *Token, u User) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	stmt := `
+	// delete existing tokens
+	stmt := `delete from tokens where user_id = ?`
+	_, err := m.DB.ExecContext(ctx, stmt, u.ID)
+	if err != nil {
+		return err
+	}
+
+	// create a new token
+	stmt = `
 		INSERT INTO tokens
 		(user_id, name, email, token_hash, created_at, updated_at)
 		VALUES (?, ?, ?, ?, ?, ?)
 	`
 
-	_, err := m.DB.ExecContext(ctx, stmt,
+	_, err = m.DB.ExecContext(ctx, stmt,
 		u.ID,
 		u.LastName,
 		u.Email,
