388e8155d032357bcbd3cfbfc343d8bde0f7b4cb
kons77 <staneua@gmail.com>
2025-03-02 20:43:29 +0200

    Set up the reset password page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -368,13 +368,17 @@ func (app *application) ShowResetPassword(w http.ResponseWriter, r *http.Request
 	}
 
 	valid := signer.VerifyToken(testURL)
-	if valid {
-		w.Write([]byte("valid"))
-	} else {
-		w.Write([]byte("invalid"))
+	if !valid {
+		app.errorLog.Println("Invalid url - tampering detected")
+		return
 	}
 
-	if err := app.renderTemplate(w, r, "reset-password", &templateData{}); err != nil {
+	data := make(map[string]interface{})
+	data["email"] = r.URL.Query().Get("email")
+
+	if err := app.renderTemplate(w, r, "reset-password", &templateData{
+		Data: data,
+	}); err != nil {
 		app.errorLog.Println(err)
 	}
 }
